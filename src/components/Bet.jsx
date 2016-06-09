508520a30515bae0b87c887238867ff63e26afac
odin <mousnier.guillaume@gmail.com>
2016-06-09 07:45:25 +0200

    AJout du checkDate

--- src/components/Bet.jsx
+++ src/components/Bet.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import moment from 'moment';
 
 function mergeBet(event, oldBet, match) {
     const score = Number.parseInt(event.target.value.trim(), 0);
@@ -11,8 +12,14 @@ function mergeBet(event, oldBet, match) {
     });
 }
 
+function checkDate(matchDate) {
+    return moment() >= moment(matchDate).subtract(1, 'hour');
+}
+
 export default function Bet(props) {
     const { match, bet, handleChange } = props;
+    const betIsClosed = checkDate(match.date);
+    const onChange = betIsClosed ? () => {} : (event) => handleChange(mergeBet(event, bet, match));
     return (
         <div style={{display: 'flex', flexDirection: 'row'}}>
             <label>{match.team1.name}</label>
@@ -20,12 +27,14 @@ export default function Bet(props) {
                 name="team1"
                 type="number"
                 value={Number.isInteger(bet.team1.score) ? bet.team1.score : ''}
-                onChange={(event) => handleChange(mergeBet(event, bet, match))} />
+                onChange={onChange}
+                disabled={betIsClosed} />
             <input
                 name="team2"
                 type="number"
                 value={Number.isInteger(bet.team1.score) ? bet.team2.score : ''}
-                onChange={(event) => handleChange(mergeBet(event, bet, match))} />
+                onChange={onChange}
+                disabled={betIsClosed} />
             <label>{match.team2.name}</label>
             { Number.isInteger(bet.team2.score) && Number.isInteger(bet.team1.score) ? <div style={{color: 'green'}}>Validated</div> : '' }
         </div>
